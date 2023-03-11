32412336f10cd25bfe2e03c80e9c2a4fa8af0f73
Alvaro Correa <u21211558@utp.edu.pe>
2023-03-11 16:52:53 -0500

    Clases Entidades Creadas y DAOs en proceso

--- src/main/java/pe/edu/utp/Models/DAO/EstudianteDAO.java
+++ src/main/java/pe/edu/utp/Models/DAO/EstudianteDAO.java
@@ -1,4 +1,34 @@
 package pe.edu.utp.Models.DAO;
 
+import pe.edu.utp.Models.DTO.EstudianteDTO;
+import pe.edu.utp.utils.ErrorLog;
+import pe.edu.utp.utils.TextUTP;
+
+import java.io.IOException;
+
 public class EstudianteDAO {
+
+    public static EstudianteDTO listarEstudiantes(String path_file, ErrorLog errorLog){
+        EstudianteDTO[] listaEstudiantes;
+
+        try {
+            String[] datos = TextUTP.readlinesAsArray(path_file, TextUTP.OS.WINDOWS);
+
+            listaEstudiantes = new EstudianteDTO[datos.length-1];
+
+            EstudianteDTO estudianteDTO;
+            for (int i=0; i < listaEstudiantes.length; i++){
+
+            }
+
+        } catch (Exception e) {
+            try {
+                String  event = errorLog.log(e.getMessage(), ErrorLog.Level.ERROR);
+                System.out.print(event);
+            }catch (Exception ex){
+                System.out.print("Error al registrar logs");
+            }
+        }
+
+    }
 }
--- src/main/java/pe/edu/utp/Models/DTO/EstudianteDTO.java
+++ src/main/java/pe/edu/utp/Models/DTO/EstudianteDTO.java
@@ -1,4 +1,101 @@
 package pe.edu.utp.Models.DTO;
 
 public class EstudianteDTO {
+
+    // region [Attributes]
+    String codigoEstudiante;
+    String contrasena;
+    String primerNombre;
+    String segundoNombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    String carrera;
+    String planDeEstudios;
+    // endregion
+
+    // region [Getters & Setters]
+
+    public String getCodigoEstudiante() {
+        return codigoEstudiante;
+    }
+
+    public void setCodigoEstudiante(String codigoEstudiante) {
+        this.codigoEstudiante = codigoEstudiante;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getPlanDeEstudios() {
+        return planDeEstudios;
+    }
+
+    public void setPlanDeEstudios(String planDeEstudios) {
+        this.planDeEstudios = planDeEstudios;
+    }
+
+    // endregion
+
+    // region [Contructors]
+
+    public EstudianteDTO(String codigoEstudiante, String contrasena,
+                         String primerNombre, String segundoNombre,
+                         String apellidoPaterno, String apellidoMaterno, String carrera,
+                         String planDeEstudios) {
+        this.codigoEstudiante = codigoEstudiante;
+        this.contrasena = contrasena;
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.carrera = carrera;
+        this.planDeEstudios = planDeEstudios;
+    }
+
+    // endregion
 }
